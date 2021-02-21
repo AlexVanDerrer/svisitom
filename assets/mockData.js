95d7d6eb9d9ef9818d046f7057d70adcbacd22c6
AlexVanDerrer <borisow.a.s@yandex.ru>
2021-02-21 14:57:43 +0300

    добавляет тестовые данные

---
+++ assets/mockData.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        "id": 0,
+        "name": "quis culpa ad",
+        "date": "2019-01-15T06:57:35 -03:00",
+        "status": true
+    },
+    {
+        "id": 1,
+        "name": "ullamco enim excepteur",
+        "date": "2020-07-17T09:05:15 -03:00",
+        "status": true
+    },
+    {
+        "id": 2,
+        "name": "mollit proident in",
+        "date": "2016-06-16T01:41:30 -03:00",
+        "status": true
+    },
+    {
+        "id": 3,
+        "name": "irure dolor laborum",
+        "date": "2014-03-10T04:05:54 -04:00",
+        "status": true
+    },
+    {
+        "id": 4,
+        "name": "exercitation sint est",
+        "date": "2015-04-28T11:16:25 -03:00",
+        "status": false
+    },
+    {
+        "id": 5,
+        "name": "culpa voluptate ullamco",
+        "date": "2016-09-21T04:52:55 -03:00",
+        "status": false
+    },
+    {
+        "id": 6,
+        "name": "cillum anim commodo",
+        "date": "2019-10-22T04:29:21 -03:00",
+        "status": false
+    },
+    {
+        "id": 7,
+        "name": "qui anim do",
+        "date": "2018-07-01T08:46:35 -03:00",
+        "status": true
+    },
+    {
+        "id": 8,
+        "name": "consectetur deserunt dolore",
+        "date": "2018-07-26T07:39:03 -03:00",
+        "status": true
+    },
+    {
+        "id": 9,
+        "name": "officia eiusmod anim",
+        "date": "2017-01-27T05:53:42 -03:00",
+        "status": true
+    }
+]
--- pages/tablePage.vue
+++ pages/tablePage.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-      table
+  <div class="container">
+      <div class="Table--table-container">
+            <b-table striped hover :items="items"></b-table>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
+    middleware: ['checkAuth'],
+    async fetch(){
+        try {
+            await this.$store.dispatch('getData')
+        } catch (error) {
+            console.error(error);
+            throw error
+        }
+    },
+    data() {
+      return {}
+    },
+    computed: {
+        items: {
+            get(){
+                return this.$store.getters.getData
+            },
+            set(data){
+                this.$store.commit('setData', data)
+            }
+        }
+    },
+    async mounted(){
 
+    }
 }
 </script>
 
 <style>
+
 
 </style>
