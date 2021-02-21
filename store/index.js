3bcc8d822adaa891c13e9b2601e44e6456e886ab
AlexVanDerrer <borisow.a.s@yandex.ru>
2021-02-21 14:56:16 +0300

    добавляет vuex

---
+++ store/index.js
@@ -0,0 +1,28 @@
+import DATA from '@/assets/mockData'
+export const strict = false
+
+export const state = () => {
+    return { 
+        token: null,
+        data: [] 
+    } 
+}
+
+export const getters = {
+    isAuth: state => !!state.token,
+    getData: state => state.data
+}
+
+export const mutations = {
+    setToken: (state, data) => (state.token = data),
+    setData: (state, data) => (state.data = data) 
+}
+
+export const actions = {
+    getData(context){
+        // имитирует запрос на сервер
+        setTimeout(()=>{
+            context.commit('setData', DATA)
+        },500)
+    }
+}
